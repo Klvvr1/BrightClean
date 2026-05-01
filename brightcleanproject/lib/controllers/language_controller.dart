0e30ab6f8cc5c8c0f0d5eb7859e171f4e52f0bf5
ahmed albatati <aaadb911@gmail.com>
2026-05-01 20:37:22 +0300

    feat: implement localization support with LanguageController and configure MaterialApp with locale management

---
+++ brightcleanproject/lib/controllers/language_controller.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LanguageController {
+  // Singleton pattern to ensure only one instance exists
+  static final LanguageController _instance = LanguageController._internal();
+
+  factory LanguageController() {
+    return _instance;
+  }
+
+  LanguageController._internal();
+
+  // ValueNotifier to hold the current locale and notify listeners of changes
+  final ValueNotifier<Locale> locale = ValueNotifier<Locale>(const Locale('ar', 'AE'));
+
+  // Method to change the language
+  void changeLanguage(Locale newLocale) {
+    locale.value = newLocale;
+  }
+}
